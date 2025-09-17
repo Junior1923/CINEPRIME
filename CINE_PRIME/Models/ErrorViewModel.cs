b7422f113f0076b4a4e38c908a48d62142f1e31d
MSI-KATANAGF66\junio <junior.musseb@gmail.com>
2025-09-17 00:13:53 -0400

    subida de entorno

---
+++ CINE_PRIME/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CINE_PRIME.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
