d10124929f56864cb7376138be8a5e0b42738bc2
Jean Luis De Los Santos <jeanluisdelossantosgarabito@gmail.com>
2025-10-19 01:38:51 -0400

    Agregando estilos

--- CINE_PRIME/obj/Debug/net9.0/CINE_PRIME.AssemblyInfo.cs
+++ CINE_PRIME/obj/Debug/net9.0/CINE_PRIME.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CINE_PRIME")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+106957d7dcf71d98fddf8b4e4228f099a19ac236")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de47242ba4009c66fcfee0845bc7530e1b6496b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CINE_PRIME")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CINE_PRIME")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
