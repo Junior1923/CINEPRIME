bcf96312ec2773c462a2c737a57439118ec645b4
Jean Luis De Los Santos <jeanluisdelossantosgarabito@gmail.com>
2025-12-10 22:26:16 -0400

    Final

--- CINE_PRIME/obj/Debug/net9.0/CINE_PRIME.AssemblyInfo.cs
+++ CINE_PRIME/obj/Debug/net9.0/CINE_PRIME.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CINE_PRIME")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d54c8f1f26362253e4bcd0da4221ebe73576da4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+463ad71021a5e92bf3b0be51eab893b642bee13d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CINE_PRIME")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CINE_PRIME")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
